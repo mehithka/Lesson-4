1b3ed11fa048a7eac21ca71065c98c9a2971452d
mehithka <mehith.karacasia@gmail.com>
2024-12-11 15:22:09 +0500

    lesson 4 activity one

---
+++ activity1.py
@@ -0,0 +1,11 @@
+maths = 78
+science = 99
+computer = 100
+english = 90
+
+print("marks of each subject:" ,"\n", "maths:",maths ,"\n", "science:", science ,"\n", "computer: ", computer,"\n","english: ", english)
+sum = maths+science+computer+english 
+print("\n","the total marks scored are: " ,sum)
+
+average = sum/4 
+print("the average of the student across all the subjects is: " , average)
