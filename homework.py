4629a952967e800eae1187d6eb62c7a24cfc0871
mehithka <mehith.karacasia@gmail.com>
2024-12-11 15:57:33 +0500

    Homework

---
+++ homework.py
@@ -0,0 +1,8 @@
+num= 49
+numsqrt=(num)
+
+print(f'The Number is {num} and it is stored in numsqrt {numsqrt}')
+
+num = int(input("enter the number:"))
+numsqrt = num ** 0.5
+print('The square root of %0.2f is %0.3f'%(num ,numsqrt))
